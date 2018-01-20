a1309cc6cac32dcbbd4e27b234ba701850352a1d
Noah Garfinkle <noah.garfinkle@gmail.com>
2018-01-20 15:56:38 -0600

    starting to organize code

---
+++ SpatialIO.py
@@ -0,0 +1,29 @@
+from enum import Enum
+class CRS(Enum):
+    WGS84 = 4326
+    WMAS = 3857
+
+
+class RasterLayer:
+    def __init__(self):
+        return None
+
+class VectorLayer:
+    def __init_(self):
+        return None
+
+class PointLayer:
+    def __init_(self):
+        return None
+
+class LineLayer:
+    def __init_(self):
+        return None
+
+class PolygonLayer:
+    def __init_(self):
+        return None
+
+class Map:
+    def __init__(self):
+        return None
