9907cce8a57c9613e6df8adebe604d6685debeef
Noah Garfinkle <noah.garfinkle@gmail.com>
2018-01-21 13:51:11 -0600

    Added support for postgis and folium mapping

--- SpatialIO.py
+++ SpatialIO.py
@@ -22,6 +22,14 @@ __python_version__ = "2.7"
 __date_created__ = "20 January 2018"
 
 import doctest
+import folium
+import folium.plugins as plugins
+import numpy as np
+import geopandas as gpd
+from datetime import datetime, timedelta
+from shapely.geometry import Point
+import psycopg2
+from shapely.geometry import Point, Polygon
 
 
 from enum import Enum
@@ -29,15 +37,77 @@ class CRS(Enum):
     WGS84 = 4326
     WMAS = 3857
 
+class PostGIS:
+    def __init__(self,dbname,user='postgres',host='localhost',password='postgres'):
+        connString = "dbname='%s' user='%s' host='%s' password='%s'" %(dbname,user,host,password)
+        try:
+            self.conn = psycopg2.connect(connString)
+        except:
+            self.conn = None
+            print "Unable to connect to database"
+
+    def query(self,sql):
+        cur = self.conn.cursor()
+        cur.execute(sql)
+        rows = cur.fetchall()
+        return rows
+
+    def queryToDF(self,sql,geom_col='geom'):
+        try:
+            df = gpd.read_postgis(sql,con=self.conn,geom_col=geom_col)
+            return df
+        except:
+            print "Unable to execute query"
+
+
+class GeoDataFrame:
+    def __init__(self):
+        self.df = None
+        self.name = "Not Set"
+        self.type = "Point"
+
+    def createGeoDataFrame(self,crs,columns=['geometry']):
+        crsDict = {'init':'epsg:%s' %(crs.value)}
+        self.df = gpd.GeoDataFrame(crs=crsDict,columns=columns)
+
+    def addColumn(self,colName):
+        self.df[colName] = None
+
+    def addRow(self,mapping):
+        self.df = self.df.append(mapping,ignore_index=True)
+
+    def plot(self):
+        self.df.plot()
+
+g = GeoDataFrame()
+g.createGeoDataFrame(CRS.WMAS,columns=['geometry','a','b'])
+g.addRow({'geometry':Point(49,50),'a':1,'b':'c'})
+g.plot()
 
 class RasterLayer:
     def __init__(self):
         return None
 
+    def crop(self,lx,ly,ux,uy):
+        result = RasterLayer()
+        return result
+
 
 class VectorLayer:
     def __init_(self):
-        return None
+        self.df = None
+
+    def loadFeatureLayerFromFile(self,filePath):
+        """
+        Support shapefile, geojson
+        """
+        self.df = gpd.read_file(filePath)
+
+    def loadFeatureLayerFromPostGIS(self,con,sql,geom_col='geom',crs=None,index_col=None,coerce_float=True,params=None):
+        """
+
+        """
+        self.df = gpd.read_postgis(sql,con,geom_col=geom_col,crs=crs,index_col=index_col,params=params)
 
 
 class PointLayer:
@@ -57,7 +127,7 @@ class PolygonLayer:
 
 class Map:
     def __init__(self):
-        return None
+        self.map = folium.Map([48., 5.], tiles='stamentoner', zoom_start=6)
 
     def addRasterLayerAsOverlay(self,rasterLayer):
         # http://qingkaikong.blogspot.in/2016/06/using-folium-5-image-overlay-overlay.html
@@ -69,8 +139,13 @@ class Map:
         # http://andrewgaidus.com/leaflet_webmaps_python/
         return 0
 
+    def saveMap(self,filePath):
+        self.map.save(filePath)
+        print("Map saved to %s" %(filePath))
+
 """
 Manages all test functions for SpatialIO
-"""
+"""output_file("burtin.html", title="burtin.py example")
+
 def test():
     doctest.testmod()
