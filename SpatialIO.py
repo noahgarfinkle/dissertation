7279d40b1fc2ac422f410a0c77663c26fb50ecc4
Noah Garfinkle <Noah.W.Garfinkle@usace.army.mil>
2018-03-04 17:36:58 -0600

    Added code to hopefully write a geopandas dataframe into the ENSITE database!!!

--- SpatialIO.py
+++ SpatialIO.py
@@ -32,7 +32,7 @@ import numpy as np
 import geopandas as gpd
 from datetime import datetime, timedelta
 import psycopg2
-from shapely.geometry import Point, Polygon
+from shapely.geometry import Point, Polygon, LineString, MultiLineString
 from enum import Enum
 import matplotlib.pyplot as plt
 from osgeo import gdal, ogr, osr
@@ -45,12 +45,13 @@ from numpy import logical_and
 from numpy import zeros
 from numpy import uint8
 import scipy
-import os
 from PIL import Image, ImageChops
 import rasterio
 import fiona
 from rasterio import features
 import os
+import pandas as pd
+import sys
 
 ## HELPFUL FOR DEBUGGING
 # %matplotlib inline
@@ -977,7 +978,110 @@ def rasterizeGeodataFrameColumn(df,column,outputRasterPath,resolution=30,crs=Non
     return outputRasterPath
 
 
+def dataFrameToENSITEDatabase(df,studyID,layerName,layerID=None):
+    """ Writes a Vector GeoDataFrame into the ENSITE database
 
+    This code is mirrored from the ENSITE ImportExportLibrary, and is necessary
+    to allow site search to directly insert into the database.  Site Search
+    results will be written to the database as a single vector layer per
+    study objective.
+
+    Args:
+        df (GeoPandas GeoDataFrame): A vector GeoDataFrame with CRS set
+        studyID (int): The ID for the ENSITE study this layer belongs to.
+        layerName (str): The pretty-print name to refer to this layer as
+        layerID (int): Passed if a layer has already been creaed by the ENSITE
+            user interface, otherwise this is sest during insertion
+
+    Returns:
+        None
+
+    Raises:
+        None
+
+    Todo:
+        * Lots of testing
+        * Evaluate security
+
+    Tests:
+        None
+    """
+    con = psycopg2.connect(database="ensite", user="postgres",password="postgres",host="127.0.0.1")
+    cur = con.cursor()
+    # get the projection of the df
+    try:
+		projection = df.crs['init']
+    except:
+		projection = "epsg:3857" # kludge
+
+
+    # insert the layer
+    geometryType = df.loc[0]["geometry"].type #TODO try catch type thing
+
+    if not layerID:
+        insertStatement = "INSERT INTO ensite_layer (ensite_study_id,projection,name, geometry_type,primary_color,secondary_color) VALUES (%s,'%s','%s', '%s','rgba(0,0,0,.6)', 'rgba(150,150,150,.25)') RETURNING id;" %(studyID,projection,layerName, geometryType) # todo, make sure studyID remains an integer
+        cur.execute(insertStatement)
+        layerID = cur.fetchone()[0]
+
+
+    # set up the columns
+    columns = [column for column in df.columns if column != "geometry"]
+    columnTypes = {}
+    for column in columns:
+        dtype = df[column].dtype.name
+        if dtype == 'object':
+            dtype = 'text'
+        elif dtype =='float64':
+            dtype = 'real'
+        columnTypes[column] = dtype
+    columnKeys = {}
+    for columnName,columnType in columnTypes.iteritems():
+        insertStatement = "INSERT INTO ensite_feature_property_name (name,type,ensite_layer_id) VALUES ('%s','%s',%s) RETURNING id;" %(columnName,columnType,layerID)
+        cur.execute(insertStatement)
+        columnID = cur.fetchone()[0]
+        columnKeys[columnName] = columnID
+        #con.commit()
+
+    # insert the features and their properties one at a time
+    for i,row in df.iterrows():
+        # write the feature
+        geometry = row["geometry"]
+        geometryType = geometry.type
+
+        geometryTable = "ensite_feature_point"
+        if geometryType != "Point":
+            geometryTable = "ensite_feature_vector"
+        insertStatement = "INSERT INTO ensite_feature (ensite_layer_id,type) VALUES (%s,'%s') RETURNING id;" %(layerID,geometryTable)
+        cur.execute(insertStatement)
+        featureID = cur.fetchone()[0]
+        #con.commit()
+
+        # write the feature geometry
+        geometry_wkt = geometry.wkt
+        insertStatement = "INSERT INTO %s (ensite_feature_id,geometry) VALUES (%s,ST_GeometryFromText('%s',%s));" %(geometryTable,featureID,geometry_wkt,projection.split(':')[1])
+#        insertStatement = "INSERT INTO feature_point (feature_id,geometry) VALUES (%s,ST_SetSRID(%s::geometry,%s) RETURNING id" %(featureID,geometry_wkb,4326)
+        cur.execute(insertStatement)
+        #con.commit()
+
+        for columnName,columnKey in columnKeys.iteritems():
+            # write the properties
+            valueType = columnTypes[columnName]
+            columnID = columnKeys[columnName]
+            columnValue = row[columnName]
+            # clean up columnValue for commit, courtesy of http://stackoverflow.com/questions/3224268/python-unicode-encode-error
+            if valueType == 'text':
+                columnValue = unicode(columnValue)
+            else:
+                columnValue = unicode(str(columnValue))
+            if columnValue:
+                insertStatment = "INSERT INTO ensite_feature_property_value (ensite_feature_id,ensite_feature_property_name_id,value,value_type) VALUES (%s,%s,'%s','%s');" %(featureID,columnID,columnValue.replace("'",""),valueType)
+            else:
+                insertStatment = "INSERT INTO ensite_feature_property_value (ensite_feature_id,ensite_feature_property_name_id,value,value_type) VALUES (%s,%s,%s,'%s');" %(featureID,columnID,"NULL",valueType)
+            cur.execute(insertStatment)
+            #con.commit()
+
+    con.commit()
+    return layerID
 
 
 ## CURRENT TEST
