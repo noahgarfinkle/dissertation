e2f139087f8d1a2321ddbde50adebb4374b12a13
Noah Garfinkle <Noah.W.Garfinkle@usace.army.mil>
2018-05-12 09:26:52 -0500

    Cleaning up the spatial opt code to implement

--- SpatialOpt.py
+++ SpatialOpt.py
@@ -1,21 +1,48 @@
+# -*- coding: utf-8 -*-
 """
-# https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.linprog.html
-c = [-1, 4]
-A = [[-3, 1], [1, 2]]
-b = [6, 4]
-x0_bounds = (None, None)
-x1_bounds = (-3, None)
-from scipy.optimize import linprog
-res = linprog(c, A_ub=A, b_ub=b, bounds=(x0_bounds, x1_bounds),
-              options={"disp": True})
-print(res)
-
-Implements a Stackelberg Game where player 1 chooses to invest resources to
-defend a node and player 2 then invests resources to attack a node.  If player
-1 is defending the attacked node, damage is mitigated.  Utility is scored by
-routing capabilities
-
-def competitiveRouting():
-    return 0
+Manages simple objective functions for vector data layers
 """
-print "Still need to implement spatial opt"
+
+__author__ = "Noah W. Garfinkle"
+__copyright__ = "Copyright 2018, Noah W. Garfinkle"
+__credits__ = ["Dr. Ximing Cai", "Dr. George Calfas", "Thomas 'Max' Foltz",
+                    "Juliana McMillan-Wilhoit", "Matthew Hiett",
+                    "Dylan Pasley", "Marcus Voegle", "Eric Kreiger"]
+__license__ = "GPL"
+__version__ = "0.0.1"
+__version_dinosaur__ = "Apotosauras"
+__maintainer__ = "Noah Garfinkle"
+__email__ = "garfink2@illinois.edu"
+__status__ = "Development"
+__python_version__ = "2.7"
+__date_created__ = "10 May 2018"
+
+## IMPORTS
+from lxml import etree as ET
+import gdaltools as gdt
+from osgeo import gdal, osr, ogr
+import pandas as pd
+import geopandas as gpd
+import fiona
+import pyproj
+import rasterio
+import numpy as np
+import numpy.ma as ma
+import math
+import shapely
+from shapely.geometry import Point, Polygon
+from shapely.wkt import loads
+import shapely.geometry as geom
+import shapely.affinity
+from rasterstats import zonal_stats, raster_stats, point_query, utils
+import matplotlib.pyplot as plt
+import datetime
+import time
+
+import CandidateDataFrameOperations as candidates
+import ENSITEIO as eio
+import Objective_Analytic as objective_analytic
+import Objective_Raster as objective_raster
+import pgdissroute as pgdissroute
+import SpatialIO as io
+import SpatialOpt as opt
